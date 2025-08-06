09b2a02dbc3447b5bf2cc76fe3eeb483b30b576f
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-05 23:10:47 -0600

    lab1

---
+++ lab-1/program/Driver.py
@@ -0,0 +1,15 @@
+import sys
+from antlr4 import *
+from MiniLangLexer import MiniLangLexer
+from MiniLangParser import MiniLangParser
+
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = MiniLangLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = MiniLangParser(stream)
+    tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
+    print(tree.toStringTree(recog=parser))
+
+if __name__ == '__main__':
+    main(sys.argv)
